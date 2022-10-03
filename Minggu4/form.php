3455c59f4fe7c91bf5422df82cba7548f15bb1bc
salsaseva <113366125+salsaseva@users.noreply.github.com>
2022-10-03 23:12:36 +0700

    Add files via upload

---
+++ Minggu4/form.php
@@ -0,0 +1,21 @@
+<?php
+$nama=$_POST['nama'];
+$tempatlahir=$_POST['tempatlahir'];
+$tanggal=$_POST['tanggal'];
+$agama=$_POST['agama'];
+$alamat=$_POST['alamat'];
+$nomerhp=$_POST['nomerhp'];
+$jk=$_POST['jk'];
+$hobi=$_POST['hobi'];
+$foto=$_POST['foto'];
+
+echo "Nama : ".$nama."<br>";
+echo "Tempat Lahir : ".$tempatlahir."<br>";
+echo "Tanggal Lahir : ".$tanggal."<br>";
+echo "Agama : ".$agama."<br>";
+echo "Alamat : ".$alamat."<br>";
+echo "Nomor HP : ".$nomerhp."<br>";
+echo "Jenis Kelamin : ".$jk."<br>";
+echo "Hobi : ".$hobi."<br>";
+echo "File : ".$foto."<br>";
+?>
